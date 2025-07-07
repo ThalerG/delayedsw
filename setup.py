ce6e6ac254af3988757576e15a996eb4fbd03baf
ThalerG <thalergabriel@gmail.com>
2025-07-07 19:26:49 -0300

    build: bump 0.1.1 -> 0.1.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='delayedsw',
-    version='0.1.1',
+    version='0.1.2',
     description='Delayed space sliding window transformer',
     author='Gabriel Thaler',
     packages=find_packages(),
     install_requires=[
-        'scikit-learn>=1.0.0',
+        'scikit-learn>=1.6.0',
         'numpy'
     ],
 )
