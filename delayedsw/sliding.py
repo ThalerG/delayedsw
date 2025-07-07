9cf2b432eeb681129e5e8250f0bd8c05c3797563
ThalerG <thalergabriel@gmail.com>
2025-07-07 16:52:49 -0300

    refactor: refactor code to be more akin to sklearn template

--- delayedsw/sliding.py
+++ delayedsw/sliding.py
@@ -1,108 +1,104 @@
 """Class for delayed sliding window transformers"""
 
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin, _fit_context
+from sklearn.utils.validation import check_is_fitted, validate_data
+from sklearn.utils._param_validation import Interval
+from numbers import Integral
 import numpy as np
 
-class DelayedSlidingWindow(BaseEstimator, TransformerMixin):
+class DelayedSlidingWindow(TransformerMixin, BaseEstimator):
+
+
+    _parameter_constraints = {
+        "window_size": [Interval(Integral, 1, None, closed="left")],  # Must be a positive integer
+        "delay_space": [Interval(Integral, 1, None, closed="left")],  # Must be a positive integer
+        "columns_to_transform": [list, None],  # Must be a list of column names or indices or None
+        "order_by": [str, int, None],  # Must be a string or int
+        "split_by": [str, int, None],  # Must be a string or int
+        "drop_nan": ["boolean"],  # Must be a boolean
+    }
+
     def __init__(self, window_size:int =1, delay_space: int=1, columns_to_transform: list[str|int]|None =None, 
-                 feature_names_in: list[str]|None = None, n_features_in: int|None = None, 
                  order_by: int|str|None = None, split_by: int|str|None = None, drop_nan: bool = True):
         """Initialize the DelayedSlidingWindow transformer."""
 
-        if not isinstance(window_size, int) or window_size <= 0:
-            raise ValueError("window_size must be a positive integer.")
-        if not isinstance(delay_space, int) or delay_space < 0:
-            raise ValueError("delay_space must be a non-negative integer.")
-        if columns_to_transform is not None and not isinstance(columns_to_transform, list):
-            raise ValueError("columns_to_transform must be a list or None.")
-        if columns_to_transform is not None and not all(isinstance(col, (int, str)) for col in columns_to_transform):
-            raise ValueError("All elements in columns_to_transform must be either integers or strings.")
-        if feature_names_in is not None and not isinstance(feature_names_in, list):
-            raise ValueError("feature_names_in must be a list or None.")
-        if n_features_in is not None and (not isinstance(n_features_in, int) or n_features_in <= 0):
-            raise ValueError("n_features_in must be a positive integer or None.")
-        if order_by is not None and not isinstance(order_by, (int, str)):
-            raise ValueError("order_by must be an integer, string, or None.")
-        if split_by is not None and not isinstance(split_by, (int, str)):
-            raise ValueError("split_by must be an integer, string, or None.")
-
-        
         self.window_size = window_size
         self.delay_space = delay_space
-        self.feature_names_in = feature_names_in
-        self.n_features_in = n_features_in
         self.columns_to_transform = columns_to_transform
         self.order_by = order_by
         self.split_by = split_by
         self.drop_nan = drop_nan
 
+    @_fit_context(prefer_skip_nested_validation=True)
     def fit(self, X, y=None):
         """Get column names and number of features from the input data."""
 
         # Store original input information for output formatting
-        self._check_input(X)
-
         self.input_type_ = type(X)
+        
+        if y is not None:
+            (X, y) = validate_data(self,X, y, accept_sparse=True, reset = True)
+        else:
+            X = validate_data(self, X, accept_sparse=True, reset = True)
+
+        self._check_input(X)
             
         return self
     
     def _check_input(self, X):
         """Check and set input data attributes."""
         
-        if hasattr(X, 'shape'):
-            self.n_features_in_ = X.shape[1]
-        else:
-            raise ValueError("Input data must have a shape attribute (e.g., a numpy array or pandas DataFrame).")
-        
-        n = X.shape[0]
-        if n < self.window_size:
+        n_rows = X.shape[0]
+        if n_rows < self.window_size:
             raise ValueError("Input array length must be at least as large as window_size.")
         
-        if self.feature_names_in is None:
-            if hasattr(X, 'columns'):
-                self.feature_names_in_ = X.columns.tolist()
-            else:
-                self.feature_names_in_ = [f"feature_{i}" for i in range(X.shape[1])]
-
-        if self.columns_to_transform is not None:
-            if not all(isinstance(col, int) and 0 <= col < self.n_features_in_ for col in self.columns_to_transform): # Column names are not integer indices
-                if hasattr(X, 'columns'):
-                    # X is a pandas DataFrame, columns_to_transform should be column names (strings)
-                    if not all(col in X.columns for col in self.columns_to_transform):
-                        # If any column name is not in the DataFrame's columns, raise an error
-                        raise ValueError("All columns in columns_to_transform must be either valid indices or valid column names of the input data.")
-                    else:
-                        # Convert column names to indices
-                        self.columns_indices_ = [X.columns.get_loc(col) for col in self.columns_to_transform]
+        if self.columns_to_transform is not None: # Check if specific columns are provided
+            if all(isinstance(col, int) for col in self.columns_to_transform):  # If all columns are indices
+                if not all(0 <= col < self.n_features_in_ for col in self.columns_to_transform):
+                    raise ValueError("All indices in columns_to_transform must be less than the number of features in the input data.")
                 else:
-                    # X is a numpy array or similar, columns_to_transform should be indices
-                    raise ValueError("All columns in columns_to_transform must be valid indices of the input data.")
+                    # If columns_to_transform contains valid indices, use them directly
+                    self.columns_indices_ = self.columns_to_transform
+                    self.columns_in_str = [str(col) for col in self.columns_to_transform]
             else:
-                # If columns_to_transform are valid indices, use them directly
-                self.columns_indices_ = self.columns_to_transform
-
-            self.feature_names_in_ = [self.feature_names_in_[i] for i in self.columns_indices_]
-
+                # If columns_to_transform contains strings, check if they are valid column names
+                if not all(isinstance(col, str) for col in self.columns_to_transform):
+                    raise ValueError("All elements in columns_to_transform must be either valid indices or valid column names of the input data.")
+                else:
+                    # If columns_to_transform contains strings, check if they are valid column names
+                    if hasattr(self, 'feature_names_in_'):
+                        if not all(col in self.feature_names_in_ for col in self.columns_to_transform):
+                            raise ValueError("All column names in columns_to_transform must be valid names of the input data.")
+                        else:
+                            # Convert column names to indices
+                            temp_columns = list(self.feature_names_in_)
+                            self.columns_indices_ = [temp_columns.index(col) for col in self.columns_to_transform]
+                            self.columns_in_str = self.columns_to_transform
+                    else:
+                        raise ValueError("If columns_to_transform contains strings, X must provide feature names.")
         else:
-            # If no specific columns are provided, transform all columns
+            # If no specific columns are provided, use all columns
             self.columns_indices_ = list(range(self.n_features_in_))
+            self.columns_in_str = [str(col) for col in self.columns_indices_]
+
+        self.feature_names_out_ = []
+        for column_in in self.columns_in_str:
+            delay_index = [k * self.delay_space for k in range(self.window_size)]
+            feature_name = [f"{column_in}_{d}" for d in delay_index]
+            self.feature_names_out_.extend(feature_name)
     
     def transform(self, X, y=None):
         """Transform the data using a sliding window with delay."""
 
-
-        if not hasattr(self, 'columns_indices_'):
-            # If columns_indices_ is not set, it means fit was not called or columns_to_transform was not specified
-            raise ValueError("The transformer has not been fitted yet. Call 'fit' before 'transform'.")
+        check_is_fitted(self, 'columns_indices_')
         
         original_index = X.index if hasattr(X, 'index') else np.arange(X.shape[0])
         if hasattr(X, 'columns'):
             input_dtypes = X.dtypes.values[self.columns_indices_]
-            X = X.values  # Convert DataFrame to numpy array if necessary
-            
+
+        X = validate_data(self, X, accept_sparse=True, reset=False)
 
         X = X[:, self.columns_indices_]
-        self.feature_names_out_ = []
 
         for i in range(X.shape[1]):
             matrix_sliding = self.sliding_1d(X[:, i])
@@ -113,11 +109,6 @@ class DelayedSlidingWindow(BaseEstimator, TransformerMixin):
                 delayedData = matrix_sliding
             else:
                 delayedData = np.concatenate((delayedData, matrix_sliding), axis=1)
-
-            # Generate feature names
-            delay_index = [k * self.delay_space for k in range(self.window_size)]
-            feature_name = [f"{self.feature_names_in_[i]}_{d}" for d in delay_index]
-            self.feature_names_out_.extend(feature_name)
 
         # Get the index of the remaining rows before dropping NaNs
         if self.drop_nan:
@@ -167,8 +158,7 @@ class DelayedSlidingWindow(BaseEstimator, TransformerMixin):
     
     def get_feature_names_out(self, input_features=None):
         """Get the feature names after transformation."""
-        if self.feature_names_in is None:
+        if self.feature_names_in_ is None:
             raise ValueError("The transformer has not been fitted yet. Call 'fit' before 'get_feature_names_out'.")
-        
         
         return self.feature_names_out_
